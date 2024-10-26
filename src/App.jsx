b04b589b76a2f8eb5686db0ca0cff36cbe0d2748
andrei <andreimandrique3@gmail.com>
2024-10-26 16:53:25 +0800

    finish add to cart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { createContext, useState } from "react";
-import { Outlet, Link } from "react-router-dom";
-
-export const ProductContext = createContext();
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import useProduct from "./useProduct";
+import { useState } from "react";
 
 export default function App() {
-  const [product, setProduct] = useState("");
+  const [product, setProduct] = useState([]);
+
+  const { data, error, loading } = useProduct();
+
+  if (error) return <p>Error</p>;
+  if (loading) return <p>Loading...</p>;
 
   return (
     <>
-      <h1>HomePage</h1>
-      <p>my order: {product}</p>
-      <div>
-        <li>
-          <Link to="/">Go to Home</Link>
-        </li>
-        <li>
-          <Link to="/product">Go to Product</Link>
-        </li>
-        <li>
-          <Link to="/checkout">Go to Checkout</Link>
-        </li>
-      </div>
-      <ProductContext.Provider value={{ product, setProduct }}>
-        <Outlet />
-      </ProductContext.Provider>
+      <Header productCount={product.length} />
+      <Outlet context={{ data, setProduct, product }} />
     </>
   );
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,18 +1,42 @@
-import { useContext } from "react";
-import { ProductContext } from "./App";
+import { useOutletContext } from "react-router-dom";
 
 export default function Product() {
-  const { product, setProduct } = useContext(ProductContext);
+  const { data, setProduct } = useOutletContext();
 
-  function addtoCart() {
-    setProduct("t shirt");
+  function addToCart(order) {
+    setProduct((prev) => [...prev, order]);
   }
+
+  const productList = data.map((product) => (
+    <div key={self.crypto.randomUUID()} className="card">
+      <img
+        src={product.image}
+        alt={product.description}
+        width={180}
+        height={200}
+      />
+      <h3>{product.title}</h3>
+      <p>{product.price}</p>
+      <button
+        onClick={() => {
+          addToCart({
+            id: self.crypto.randomUUID(),
+            image: product.image,
+            title: product.title,
+            price: product.price,
+            quantity: 1,
+          });
+        }}
+      >
+        Add to Cart
+      </button>
+    </div>
+  ));
 
   return (
     <>
       <h3>Product Page</h3>
-      <p>{product}</p>
-      <button onClick={addtoCart}>Add to Cart</button>
+      <div>{productList}</div>
     </>
   );
 }
